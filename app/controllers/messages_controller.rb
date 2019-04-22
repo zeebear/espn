425b03020608f5a83258e954d724d0827af8be82
Claire Fahy <zeebear21@gmail.com>
2019-04-21 23:50:41 -0300

    fix routes, add controller skeleton methods

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,41 @@
 class MessagesController < ApplicationController
+  before_action :find_message, only: %i[show edit update destroy]
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def find_message
+
+  end
+
+  def message_params
+
+  end
 end
