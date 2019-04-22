2db80ac75325b015e429ba62d24da68136f3a2ed
Claire Fahy <zeebear21@gmail.com>
2019-04-22 00:26:35 -0300

    add new & create & views

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
-//= require_tree .
-//= require jquery
 //= require bootstrap-sprockets
+//= require turbolinks
+//= require jquery
+//= require_tree .
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,40 +2,47 @@ class MessagesController < ApplicationController
   before_action :find_message, only: %i[show edit update destroy]
 
   def index
-
+    @messages = Message.all
   end
 
   def show
-
   end
 
   def new
-
+    @message = Message.new
   end
 
   def create
+    @message = Message.new(message_params)
 
+    redirect_to root_path if @message.save
+    return
+
+    render 'new'
   end
 
   def edit
-
   end
 
   def update
+    redirect_to root_path if @message.update(message_params)
+    return
 
+    render 'edit'
   end
 
   def destroy
-
+    @message.destroy
+    redirect_to root_path
   end
 
   private
 
   def find_message
-
+    @message = Message.find(params[:id])
   end
 
   def message_params
-
+    params.require(:message).permit(:title, :description)
   end
 end
