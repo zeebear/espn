ccfedbe930ecc4b59e5c50732b7ccd173e0aab88
Claire Fahy <zeebear21@gmail.com>
2019-04-22 01:35:50 -0300

    create comments

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,11 +9,11 @@ class MessagesController < ApplicationController
   end
 
   def new
-    @message = Message.new
+    @message = current_user.messages.build
   end
 
   def create
-    @message = Message.new(message_params)
+    @message = current_user.messages.build(message_params)
 
     redirect_to root_path if @message.save
     return
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_22_042821) do
+ActiveRecord::Schema.define(version: 2019_04_22_043531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.bigint "message_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["message_id"], name: "index_comments_on_message_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
@@ -36,5 +46,7 @@ ActiveRecord::Schema.define(version: 2019_04_22_042821) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "messages"
+  add_foreign_key "comments", "users"
   add_foreign_key "messages", "users"
 end
